a59909f4d1715d5ed1541901b6d44aa2533a7199
abaker-skudlarek <abakerskudlarek@gmail.com>
2025-03-18 01:20:33 -0700

    added board nested array to hold spaces

--- Scenes/Board/board.gd
+++ Scenes/Board/board.gd
@@ -12,19 +12,30 @@ const LETTERS := ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
 
 @export var empty_space_scene: PackedScene
 
+var _board: Array[Array]
+
 
 func _ready() -> void:
-	_spawn_spaces()
+	_initialize_board()
 
 
-func _spawn_spaces() -> void:
+func _initialize_board() -> void:
 	var spawn_pos := Vector2(START_POS_X + HORIZONTAL_OFFSET + HALF_SPACE_WIDTH, START_POS_Y + VERTICAL_OFFSET + HALF_SPACE_WIDTH) 
-	for letter: String in LETTERS:
-		for number: int in NUMBERS:
+	for x: int in range(0, len(LETTERS)):
+		_board.append([])
+		for y: int in range(0, len(NUMBERS)):
 			var space_scene: Node2D = empty_space_scene.instantiate()
 			space_scene.position = spawn_pos
-			space_scene.set_space_id(str("%s%s" % [number, letter]))
+			space_scene.set_space_id(str("%s%s" % [NUMBERS[y], LETTERS[x]]))
 			$Spaces.add_child(space_scene)
 			spawn_pos.x += (HALF_SPACE_WIDTH * 2) + HORIZONTAL_OFFSET
+			_board[x].append(space_scene)
 		spawn_pos.x = START_POS_X + HORIZONTAL_OFFSET + HALF_SPACE_WIDTH
 		spawn_pos.y += (HALF_SPACE_WIDTH * 2) + VERTICAL_OFFSET
+	# TODO: for debugging
+	var ids: Array = []
+	for n: int in range(0, len(_board)):
+		for idx: int in range(0, len(_board[n])):
+			ids.append(_board[n][idx].id)
+		print(ids)	
+		ids = []
---
+++ Scenes/Space/space.gd
@@ -0,0 +1,14 @@
+extends Node2D
+class_name Space
+
+var id: String
+
+
+func set_space_id(set_id: String) -> void:
+	id = set_id
+	$CenterContainer/Label.text = str(id)
+
+
+func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed == true:
+		print("space id = %s" % id)
